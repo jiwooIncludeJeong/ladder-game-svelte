56aba8a427fb5b09d7078f3500c4f1de09caa54b
jeongjiwoo-include <jjw89506343@gmail.com>
2021-12-16 16:58:46 +0900

    [main] first commit with @next

---
+++ src/routes/index.svelte
@@ -0,0 +1,76 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	let num = 1;
+	let select = 1;
+
+</script>
+
+<main>
+	<h1>벙커키즈 사다리타기</h1>
+	<div class="enter">
+		<p>몇 명 참여합니까?</p>
+		<input type="number" bind:value={num} placeholder="몇 명의 사용자입니까?" min={1}  >
+	</div>
+	<div class="enter">
+		<p>몇 명 당첨입니까?</p>
+		<input type="number" bind:value={select} placeholder="몇 명의 사용자입니까?" min={1}  >
+	</div>
+	<div class="buttonArea">
+		<a href="/play?num={num}&select={select}">
+			<button>
+				확인
+			</button>
+		</a>
+	</div>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    input {
+        max-width: 80px;
+        align-self: center;
+    }
+
+    button {
+        width:200px;
+        background-color: #ff3e00;
+        color:white;
+        border: 1px solid white;
+    }
+
+    .enter {
+        display: flex;
+        flex-direction: column;
+    }
+    .buttonArea {
+        margin-top: 100px;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+		@media only screen and (max-width: 640px){
+        h1 {
+            font-size:1.8em;
+        }
+		}
+</style>
---
+++ src/routes/play.svelte
@@ -0,0 +1,48 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import {onMount} from "svelte";
+
+
+</script>
+
+<main>
+	<h1>두구두구</h1>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
